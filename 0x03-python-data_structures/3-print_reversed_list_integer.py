3dcb1f25bdd8e3477813a3d39edf0017ac5250da
utukJ <utukphd@gmail.com>
2022-06-05 09:01:35 +0100

    completed task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints integers in list"""
+    for i in my_list[::-1]:
+        print("{:d}".format(i))
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_reversed_list_integer(my_list)
