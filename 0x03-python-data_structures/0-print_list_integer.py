5f436c8c01090cfd08920286a474e6b98cb0a7f7
utukJ <utukphd@gmail.com>
2022-06-05 08:30:23 +0100

    first commit project 3

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
