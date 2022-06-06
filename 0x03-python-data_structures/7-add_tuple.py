1ba25031259cca8d03b48d070e92ae01cd83ea9f
utukJ <utukphd@gmail.com>
2022-06-06 22:47:29 +0100

    fixed task 7 bug

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,5 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0,)
     while len(tuple_b) < 2:
         tuple_b += (0,)
-    result = tuple_a[1] + tuple_b[1], tuple_a[2] + tuple_b[2]
-    return result
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
