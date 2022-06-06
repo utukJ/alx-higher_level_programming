b042e90543e4d63dc7c80646810ac432e3045ae8
utukJ <utukphd@gmail.com>
2022-06-06 22:18:04 +0100

    used str format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+from tkinter import W
+
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(*row, sep=' ')
+        print(("{} "*len(row))[:-1].format(*row))
+        
+
+if __name__ == "__main__":
+    matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    ]
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
