7a28d8b7033f20c7216a4502bfa13dd538f01d01
utukJ <utukphd@gmail.com>
2022-06-08 22:12:10 +0100

    appeased pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(mylist, search, replace):
-    return list(map(lambda x : replace if x == search else x, mylist))
+    return list(map(lambda x: replace if x == search else x, mylist))
