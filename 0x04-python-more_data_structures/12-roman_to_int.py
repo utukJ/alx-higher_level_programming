f7d1769199af0e8a676b619ee4865246b5e00aed
utukJ <utukphd@gmail.com>
2022-06-08 22:53:35 +0100

    fxd

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -22,20 +22,3 @@ def roman_to_int(roman_string):
         else:
             result += conv[ch]
     return result
-
-if __name__ == "__main__":
-    roman_number = "X"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "VII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "IX"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "LXXXVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "DCCVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
