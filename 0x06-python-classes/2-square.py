ec8c86b388bf66f08c2093f5ed52a20c9f0dc96e
utukJ <utukphd@gmail.com>
2022-06-21 22:29:55 +0100

    fixed minor ish

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,5 +15,5 @@ class Square:
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >=0 ")
+            raise ValueError("size must be >= 0")
         self.__size = size
