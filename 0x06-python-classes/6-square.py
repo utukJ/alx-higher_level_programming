017710b61df180514645a1c9dcc50aeb018b1ae5
utukJ <utukphd@gmail.com>
2022-06-21 23:27:45 +0100

    fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,7 +7,7 @@ This module implements a square class
 class Square:
     """Square class with input validation"""
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Args:
         size (int): size of the square, must be greater than 0
