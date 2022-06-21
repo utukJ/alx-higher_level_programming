5ab23424513d868c33878d0c81e304635c876339
utukJ <utukphd@gmail.com>
2022-06-21 23:05:10 +0100

    completed task 3-5

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module implements a square class
+"""
+
+
+class Square:
+    """Square class with input validation"""
+
+    def __init__(self, size=0):
+        """
+        Args:
+        size (int): size of the square, must be greater than 0
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns:
+            Area of the square
+        """
+        return self.__size**2
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module implements a square class
+"""
+
+
+class Square:
+    """Square class with input validation"""
+
+    def __init__(self, size=0):
+        """
+        Args:
+        size (int): size of the square, must be greater than 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Returns:
+            size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+        sets size of square to val
+
+        Args:
+            val (int): new size of the square, must be greater than 0
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns:
+            Area of the square
+        """
+        return self.__size**2
