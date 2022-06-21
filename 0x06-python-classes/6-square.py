b822888eb9868779eab2de05b5d26932ffa3a154
utukJ <utukphd@gmail.com>
2022-06-21 23:16:03 +0100

    completed task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+This module implements a square class
+"""
+
+
+class Square:
+    """Square class with input validation"""
+
+    def __init__(self, size=0, position=(0,0)):
+        """
+        Args:
+        size (int): size of the square, must be greater than 0
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """
+        Returns:
+            size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+        sets size of square to val
+
+        Args:
+            val (int): new size of the square, must be greater than 0
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        """
+        Returns:
+            position of square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        """
+        Args:
+            position (tuple: 2): size 2 tuple representing position of the square
+        """
+        if type(position) != tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        """
+        Returns:
+            Area of the square
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """
+        Prints square using '#'
+        """
+        if self.size == 0:
+            print()
+            return
+        for i in range(self.size):
+            print(" " * self.position[0], end="")
+            for j in range(self.size):
+                print("#", end="")
+            print("\n", end="")
