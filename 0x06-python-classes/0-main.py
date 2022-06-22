5008b4bde870adcb799e15922363cc8cf5a6c898
utukJ <utukphd@gmail.com>
2022-06-22 07:44:14 +0100

    dont care

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+Square = __import__('6-square').Square
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
