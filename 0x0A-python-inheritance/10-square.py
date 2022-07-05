9e7a70c90624ad88430fc4c3a71342d9377655eb
utukj <utukphd@gmail.com>
2022-07-05 13:46:10 +0100

    done

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,14 +2,14 @@
 """
 more class base
 """
-
-
-Rectangle = __import__("9-rectangle").Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Square(Rectangle):
     """defines a Square"""
     def __init__(self, size):
         """construtor method"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
