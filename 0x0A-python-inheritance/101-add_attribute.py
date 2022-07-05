85bb0c9e8870c4332d1193f50fb088a62336e47e
utukj <utukphd@gmail.com>
2022-07-05 14:32:28 +0100

    shabby

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """advanced task"""
 
+
 def add_attribute(obj, name, value):
     """adds new attribute or raises TypeError if unable"""
     if "__dict__" not in dir(type(obj)):
