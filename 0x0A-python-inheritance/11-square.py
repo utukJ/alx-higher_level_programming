d836e0d1969a5a8acb3abca654574fc93c0d573e
utukj <utukphd@gmail.com>
2022-07-05 13:48:51 +0100

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+more class base
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """defines a Square"""
+    def __init__(self, size):
+        """construtor method"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self, size):
+        return "[Square] {}/{}".format(self.__size, self.__size)
