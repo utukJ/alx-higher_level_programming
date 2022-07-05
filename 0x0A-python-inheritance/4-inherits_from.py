2780c1abdd8d235bdf0a59ad249587a39a709199
utukj <utukphd@gmail.com>
2022-07-05 09:06:25 +0100

    completed task 4 to 9

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""Task 3"""
+''' module: 4-inherits_from
+'''
 
 
 def inherits_from(obj, a_class):
-    """returns bool indicating if obj is instance of a_class"""
-    return issubclass(obj, a_class)
+    '''the object is an instance of a class that inherited (directly or indirectly)
+    obj: an object
+    a_class: a class
+    returns None
+    '''
+    return type(obj) != a_class and isinstance(obj, a_class)
