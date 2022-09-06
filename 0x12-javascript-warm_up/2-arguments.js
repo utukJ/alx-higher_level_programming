82d752dc707aa483bfc8d2112ff621bbf0f6d56f
utukj <utukphd@gmail.com>
2022-09-06 20:09:28 +0100

    fixed bug

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argc = process.argv.length;
 if (argc === 2) {
-  console.log('No argument found');
+  console.log('No argument');
 } else if (argc === 3) {
   console.log('Argument found');
 } else {
